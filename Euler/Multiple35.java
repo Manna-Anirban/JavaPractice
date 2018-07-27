a34aa689ab3601f970e0355ec15f9b518588f0a0
Manna-Anirban <amanna@cpp.edu>
2018-07-27 09:49:50 -0700

    Finds sum of all 3 and 5 multiples under 1000

---
+++ Euler/Multiple35.java
@@ -0,0 +1,27 @@
+
+public class Multiple35
+{
+    /*
+    If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    Find the sum of all the multiples of 3 or 5 below 1000
+    */
+
+    public static void main(String[] args)
+    {
+        final int LASTNUM = 1000;
+        int totalMultiple = 0;
+
+        for(int i = 1; i < LASTNUM; i++)
+        {
+            if(i % 3==0 || i %5==0)
+            {
+                totalMultiple += i;
+            }
+
+
+        }
+        System.out.println("Sum of all multiples of 3 and 5 less than 1000: "+totalMultiple);
+    }
+
+}
