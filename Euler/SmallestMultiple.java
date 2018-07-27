ca6759e0e05377ff0ae85d204ef2320160249b1a
Manna-Anirban <amanna@cpp.edu>
2018-07-27 09:37:31 -0700

    Find smallest positive number divisivble by 1-20

---
+++ Euler/SmallestMultiple.java
@@ -0,0 +1,43 @@
+public class SmallestMultiple
+{
+    /*
+        2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+        What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+     */
+
+    private void run()
+    {
+        final long lastNum = 999999999;
+        int counter = 0;
+        for(int i = 1; i < lastNum; i++)
+        {
+            for(int j = 1; j <= 20; j++)
+            {
+
+                if(i % j==0)
+                {
+                    counter++;
+                    if(counter == 20)
+                    {
+                        System.out.println(i);
+                        System.exit(0);
+                    }
+                }
+                else
+                {
+                    counter = 0;
+                }
+            }
+
+        }
+
+    }
+    public static void main(String[] args)
+    {
+
+        SmallestMultiple sm = new SmallestMultiple();
+        sm.run();
+
+    }
+}
